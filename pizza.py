6c482c86770863c05b04c94274678bfdb82e104b
suraiyapm <s.pobmark@gmail.com>
2025-10-14 17:50:21 -0400

    functional but didn't check to make sure of accuracy yet. Pretty much straight from textbook, but with added seaborn and matplotlib for visual content.

--- pizza.py
+++ pizza.py
@@ -4,10 +4,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-X, Y = np.loadtxt("pizza.txt", skiprows=1, unpack=True)
+# X, Y = np.loadtxt("pizza.txt", skiprows=1, unpack=True)
+# w = train(X,Y, iterations=100, lr=0.001)
+# print("\nw=%.10f" % w)
 
-print("X: \n ",X)
-print("Y: \n ",Y)
+# print("X: \n ",X)
+# print("Y: \n ",Y)
+
+def predict(X, w, b):
+    return X * w + b
+
+def loss(X, Y, w, b=0):
+    return np.average((predict(X,w,b)-Y)**2)
+
+
+def train(X,Y, iterations, lr):
+    w = b = 0
+    for i in range (iterations):
+        # current_loss = loss(X, Y, w, b)
+        # print("Iterations %4d => Loss: %.6f" % (i, current_loss))
+        print("Iteration %4d => Loss: %.20f" % (i, loss(X,Y,w,b)))
+        w_gradient, b_gradient = gradient(X,Y,w,b)
+        w -= w_gradient * lr
+        b -= b_gradient * lr
+    return w, b
+
+def gradient(X,Y, w,b):
+    w_gradient = 2 * np.average(X * (predict(X, w, b) - Y))
+    b_gradient = 2 * np.average(predict(X,w,b)-Y)
+    return (w_gradient, b_gradient)
+
+X,Y = np.loadtxt("pizza.txt", skiprows=1, unpack=True)
+w,b = train(X,Y, iterations=20000, lr=0.001)
+print("\nw=%.10f, b=%.10f" % (w,b))
+print("Prediction: x=%d => y=%.2f" % (20, predict(20,w,b)))
 
 sns.set()
 plt.axis([0, 50, 0, 50])
@@ -17,49 +47,3 @@ plt.xlabel("Reservations", fontsize=32)
 plt.ylabel("Pizzas", fontsize=32)
 plt.plot(X, Y, "bo")
 plt.show()
-
-def predict(X, w, b=0):
-    return X * w + b
-
-y_hat = predict(20, 2.1, 0)
-print("num pizzas: ", y_hat)
-
-def loss(X, Y, w, b=0):
-    return np.average((predict(X,w,b)-Y)**2)
-
-def train(X,Y, iterations, lr):
-    w = 0
-    for i in range (iterations):
-        current_loss = loss(X, Y, w)
-        print("Iterations %4d => Loss: %.6f" % (i, current_loss))
-        if loss(X,Y, w+lr) < current_loss:
-            w += lr
-        elif loss(X,Y, w-lr) < current_loss:
-            w -= lr
-        else:
-            return w
-    raise Exception("Couldn't cohnverge within %d iterations" % iterations)
-
-w = train(X,Y, 10000, 0.01)
-print("\nw =%.3f" % w)
-print("Prediction: x=%d => y=%.2f" % (20, predict(20,w)))
-
-def train_b(X,Y, iterations, lr):
-    w = b = 0
-    for i in range (iterations):
-        current_loss = loss(X, Y, w, b)
-        print("Iterations %4d => Loss: %.6f" % (i, current_loss))
-        if loss(X,Y, w+lr, b) < current_loss:
-            w += lr
-        elif loss(X,Y, w-lr, b) < current_loss:
-            w -= lr
-        elif loss(X,Y, w, b+lr) < current_loss:
-            b += lr
-        elif loss(X,Y,w,b-lr) < current_loss:
-            b -= lr
-        else:
-            return w, b
-    raise Exception("Couldn't converge within %d iterations" % iteration)
-
-# def gradient(X,Y,w):
-#     return 2*np.average(X*(predict(X,w,0)-Y))
